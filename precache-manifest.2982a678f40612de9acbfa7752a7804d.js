90448dbf7294c8afc079e65828eefb7bfe6f7f7d
Daniil <mytasmic@gmail.com>
2019-08-28 00:20:54 +0300

    Updates

---
+++ precache-manifest.2982a678f40612de9acbfa7752a7804d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vktestapp/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "109661d2048bb95a929e",
+    "url": "/vktestapp/static/js/runtime~main.ddc48aab.js"
+  },
+  {
+    "revision": "e6400023c50e51867db4",
+    "url": "/vktestapp/static/js/main.8aa481db.chunk.js"
+  },
+  {
+    "revision": "07cf3d4b2d1f9a6a3957",
+    "url": "/vktestapp/static/js/2.610c296e.chunk.js"
+  },
+  {
+    "revision": "e6400023c50e51867db4",
+    "url": "/vktestapp/static/css/main.fa053f26.chunk.css"
+  },
+  {
+    "revision": "07cf3d4b2d1f9a6a3957",
+    "url": "/vktestapp/static/css/2.c622599d.chunk.css"
+  },
+  {
+    "revision": "9e5e7b39ebcd3e36b0af535435255962",
+    "url": "/vktestapp/index.html"
+  }
+];
